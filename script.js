9a244e9267e5d958ccf9242e90d53462e1a5f3a4
Freonlycarvano <104436523+Freonlycarvano@users.noreply.github.com>
2022-05-10 18:27:29 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Hallo world");
+//console.error("hallo error!");
+//console.warn("Hallo warning");
+
+console.log("5 + 2");
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log(5 % 2);
+console.log("5 + 2 =" + (5+2) );
+console.log("freonly" + "carvano" + 91);
---
+++ script_var.js
@@ -0,0 +1,14 @@
+// var dinamis
+var nama = "freonly carvano";
+console.log(typeof nama);
+nama = " carvano";
+console.log("nama : * " + nama);
+
+let umur = 19;
+console.log(typeof umur);
+console.log("umur : * " + umur);
+
+// var statis / konstants
+const tgllahir ="september 91";
+console.log(tgllahir);
+tgllahir ="oktober";
